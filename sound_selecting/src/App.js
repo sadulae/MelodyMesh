e2f4d4ee76e94ba4a939217d654012a7d5c092f8
Thanuja Nipun <thanujanipun6@gmail.com>
2024-09-02 00:16:26 +0530

    crate and add Sound-selecting function main things

---
+++ sound_selecting/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
